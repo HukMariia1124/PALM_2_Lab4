73ee45ba0c710304a2d79b72b186901b25d11bd0
Mariia Guk <mashahuk27@gmail.com>
2025-04-08 23:59:33 +0300

    Add project files.

---
+++ Lab4/Mariia_Solution.cs
@@ -0,0 +1,110 @@
+﻿using System.Diagnostics;
+using System.Text;
+
+namespace Lab4
+{
+    public class MariiaSolution
+    {
+        public static void Task_1()
+        {
+            do
+            {
+                Console.WriteLine(
+                    """
+                    ------------------------------------------------------------------------------------------------------------------------
+                                                                          ВИБІР ВЕРСІЇ
+                    ------------------------------------------------------------------------------------------------------------------------
+                    1) З використанням лише типу string та додаванням чисел 1, 2, 3,..., n у кінець за допомогою оператора += типу string.
+                    2) З використанням лише типу string та додаванням чисел n, n–1,..., 2, 1 в початок за допомогою оператора + типу string.
+                    3) З використанням string, StringBuilder, та додаванням чисел 1, 2, 3,..., n у кінець за допомогою метода Append.
+                    4) З використанням string, StringBuilder, та додаванням чисел n, n–1,..., 3, 2, 1 в початок за допомогою метода Insert.
+                    0) Повернутися назад в меню.
+                    """);
+
+                byte choiceBlock = Program.Choice(4);
+                uint n = 0;
+                Stopwatch sw = Stopwatch.StartNew();
+                sw.Stop();
+                if (choiceBlock != 0)
+                {
+                    Console.Write("Введіть n: ");
+                    do
+                    {
+                        try
+                        {
+                            n = uint.Parse(Console.ReadLine()!);
+                        }
+                        catch { Program.ShowProblemMessage(); }
+                    } while (n == 0);
+                }
+
+                switch (choiceBlock)
+                {
+                    case 1:
+                        sw.Restart();
+                        Task_1_Version_1(n);
+                        sw.Stop();
+                        break;
+                    case 2:
+                        sw.Restart();
+                        Task_1_Version_2(n);
+                        sw.Stop();
+                        break;
+                    case 3:
+                        sw.Restart();
+                        Task_1_Version_3(n);
+                        sw.Stop();
+                        break;
+                    case 4:
+                        sw.Restart();
+                        Task_1_Version_4(n);
+                        sw.Stop();
+                        break;
+                    case 0:
+                        Program.Main();
+                        break;
+                    default:
+                        Program.ShowProblemMessage();
+                        break;
+                }
+                Console.WriteLine(sw);
+            } while (true);
+        }
+        static void Task_1_Version_1(uint n)
+        {
+            string res = "";
+            for (int i = 1; i <= n; i++)
+            {
+                res += i.ToString() + " ";
+            }
+            Console.WriteLine(res);
+        }
+        static void Task_1_Version_2(uint n)
+        {
+            string res = "";
+            for (uint i = n; i > 0; i--)
+            {
+                res = i.ToString() + " " + res;
+            }
+            Console.WriteLine(res);
+        }
+        static void Task_1_Version_3(uint n)
+        {
+            StringBuilder res = new StringBuilder();
+            for (int i = 1; i <= n; i++)
+            {
+                res.Append(i.ToString() + " ");
+            }
+            Console.WriteLine(res);
+        }
+        static void Task_1_Version_4(uint n)
+        {
+            StringBuilder res = new StringBuilder();
+            for (uint i = n; i > 0; i--)
+            {
+                res.Insert(0, i.ToString() + " ");
+            }
+            Console.WriteLine(res);
+        }
+    }
+}
---
+++ Lab4/Program.cs
@@ -0,0 +1,83 @@
+﻿namespace Lab4
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            do
+            {
+                Console.WriteLine(
+                    """
+                    ------------------------------------------------------------------------------------------------------------------------
+                                                                         ВИБІР ЗАВДАННЯ
+                    ------------------------------------------------------------------------------------------------------------------------
+                    ЗАВДАННЯ #1:
+                    1) MakscoldSolution.
+                    2) MariiaSolution.
+                    3) JenlastSolution.
+                    ЗАВДАННЯ #2:
+                    4) Сформувати рядок з цифр (за зростанням) та латинських літер (за спаданням) з вихідного рядка, ігноруючи інші символи.
+                    5) Реалізувати шифрування та дешифрування класичним шифром Цезаря.
+                    6) Кожен символ '+' у рядку замінити на символ '-', якщо перед '+' стоїть непарна цифра.
+                    ДОДАТКОВІ ЗАВДАННЯ:
+                    7) MakscoldSolution.
+                    8) MariiaSolution.
+                    9) JenlastSolution.
+                    0) Вийти з програми
+                    """);
+
+                byte choiceBlock = Choice(6);
+
+                switch (choiceBlock)
+                {
+                    case 1:
+                        //MakscoldSolution.Task_1();
+                        break;
+                    case 2:
+                        MariiaSolution.Task_1();
+                        break;
+                    case 3:
+                        //Jenlast_Solution.Task_1();
+                        break;
+                    case 4:
+                        //MakscoldSolution.Task_2_Form_10();
+                        break;
+                    case 5:
+                        //MariiaSolution.Task_2_Form_12();
+                        break;
+                    case 6:
+                        //Jenlast_Solution.Task_2_Form_7();
+                        break;
+                    case 7:
+                        //MakscoldSolution.Additional();
+                        break;
+                    case 8:
+                        //MariiaSolution.Additional();
+                        break;
+                    case 9:
+                        //Jenlast_Solution.Additional();
+                        break;
+                    default:
+                        ShowProblemMessage();
+                        break;
+                }
+            } while (true);
+        }
+        public static byte Choice(byte countOfBlocks = 6)
+        {
+            byte choice;
+            do
+            {
+                try
+                {
+                    choice = byte.Parse(Console.ReadLine()!);
+                    if (choice <= countOfBlocks) return choice;
+                    ShowProblemMessage();
+                }
+                catch { ShowProblemMessage(); }
+            } while (true);
+        }
+        public static void ShowProblemMessage() => Console.WriteLine("Спробуйте ще раз");
+    }
+}
